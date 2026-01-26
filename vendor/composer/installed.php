ba2629a4ce693bda5027e9c472682dde70436227
Jonathan Sandoval <jonathansandovalferreira@gmail.com>
2026-01-26 04:05:30 -0400

    Update autoload_static.php, installed.json, and installed.php for project configuration
    - Added class mappings for various application components in autoload_static.php.
    - Changed the development flag to false in installed.json and installed.php to indicate a production-ready state.
    - Updated project versioning information in installed.php to reflect the latest commit reference.

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,13 +1,13 @@
 <?php return array(
     'root' => array(
         'name' => 'evallish/quality-system',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '8318d49f146cc3b430eed025296914aaa1255922',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'dev' => true,
+        'dev' => false,
     ),
     'versions' => array(
         'dompdf/dompdf' => array(
@@ -38,9 +38,9 @@
             'dev_requirement' => false,
         ),
         'evallish/quality-system' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '8318d49f146cc3b430eed025296914aaa1255922',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
